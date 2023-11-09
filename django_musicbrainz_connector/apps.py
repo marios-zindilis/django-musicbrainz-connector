b2e0e4b9e31ce0c3c17a31bac517dfe3faf28568
Marios Zindilis <marios@zindilis.com>
2023-11-09 09:42:12 +0000

    Added boilerplate Django app code.

---
+++ django_musicbrainz_connector/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoMusicbrainzConnectorConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_musicbrainz_connector"
