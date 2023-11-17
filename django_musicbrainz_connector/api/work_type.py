fbcb68e46bc218caf72ab51f578fc7cf9b8dd0a8
Marios Zindilis <marios@zindilis.com>
2023-11-17 19:04:07 +0000

    Allow GET on WorkType by either integer ID or UUID GID fields.

--- django_musicbrainz_connector/api/work_type.py
+++ django_musicbrainz_connector/api/work_type.py
@@ -1,4 +1,9 @@
+import uuid
+
+from django.http import Http404
 from rest_framework import serializers, viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from django_musicbrainz_connector.models import WorkType
 
@@ -13,3 +18,17 @@ class WorkTypeViewSet(viewsets.ModelViewSet):
     queryset = WorkType.objects.all()
     serializer_class = WorkTypeSerializer
     http_method_names = ["get"]
+
+    def get_object(self):
+        """
+        Overriding the default `get_object` method of `ModelViewSet` to allow GET by either the MusicBrainz ID (an
+        integer) or the GID (a UUID).
+        """
+        pk = self.kwargs["pk"]
+        try:
+            uuid.UUID(pk)
+        except (AttributeError, ValueError):
+            params = {"pk": pk}
+        else:
+            params = {"gid": pk}
+        return self.serializer_class.Meta.model.objects.get(**params)
