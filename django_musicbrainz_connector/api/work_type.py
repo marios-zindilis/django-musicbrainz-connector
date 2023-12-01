c116218bda25c8453e5babcd735e0e3dcce8ba81
Marios Zindilis <marios@zindilis.com>
2023-12-01 08:36:40 +0000

    Added the ability to get a Work Type by name.

--- django_musicbrainz_connector/api/work_type.py
+++ django_musicbrainz_connector/api/work_type.py
@@ -1,6 +1,8 @@
 import uuid
+from typing import Literal
 
 from rest_framework import serializers, viewsets
+from rest_framework.exceptions import NotFound
 
 from django_musicbrainz_connector.models import WorkType
 
@@ -11,6 +13,21 @@ class WorkTypeSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+def _get_work_type_identifier_type(identifier: str | int) -> Literal["gid", "name", "id"]:
+    try:
+        uuid.UUID(identifier)
+    except (ValueError, AttributeError):
+        pass
+    else:
+        return "gid"
+    try:
+        int(identifier)
+    except ValueError:
+        return "name"
+    else:
+        return "id"
+
+
 class WorkTypeViewSet(viewsets.ModelViewSet):
     queryset = WorkType.objects.all()
     serializer_class = WorkTypeSerializer
@@ -18,14 +35,16 @@ class WorkTypeViewSet(viewsets.ModelViewSet):
 
     def get_object(self):
         """
-        Overriding the default `get_object` method of `ModelViewSet` to allow GET by either the MusicBrainz ID (an
-        integer) or the GID (a UUID).
+        Overriding the default `get_object` method of `ModelViewSet` to allow GET by any unique identifier for a Work
+        Type, either the MusicBrainz ID (an integer), or the GID (a UUID), or the name (a string). Call with something
+        like:
+
+            GET /api/work-types/Musical/
         """
         pk = self.kwargs["pk"]
+        pk_type = _get_work_type_identifier_type(pk)
+        params = {pk_type: pk}
         try:
-            uuid.UUID(pk)
-        except (AttributeError, ValueError):
-            params = {"pk": pk}
-        else:
-            params = {"gid": pk}
-        return self.serializer_class.Meta.model.objects.get(**params)
+            return self.serializer_class.Meta.model.objects.get(**params)
+        except self.serializer_class.Meta.model.DoesNotExist:
+            raise NotFound
