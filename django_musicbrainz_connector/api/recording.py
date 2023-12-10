4156025468e2f9bf9722c0827e897b582c76126a
Marios Zindilis <marios@zindilis.com>
2023-12-10 11:33:55 +0000

    Added ability to get Recordings by either ID or GID.

--- django_musicbrainz_connector/api/recording.py
+++ django_musicbrainz_connector/api/recording.py
@@ -1,4 +1,8 @@
+import uuid
+from typing import Literal
+
 from rest_framework import serializers, viewsets
+from rest_framework.exceptions import NotFound
 
 from django_musicbrainz_connector.api import DjangoMusicBrainzConnectorPagination
 from django_musicbrainz_connector.models import Recording
@@ -10,8 +14,35 @@ class RecordingSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+def _get_recording_identifier_type(identifier: str | int) -> Literal["gid", "id"]:
+    """
+    A Recording can be identified either by its ID or its GID.
+    """
+    try:
+        uuid.UUID(identifier)
+    except (ValueError, AttributeError):
+        return "id"
+    else:
+        return "gid"
+
+
 class RecordingViewSet(viewsets.ModelViewSet):
     queryset = Recording.objects.all()
     serializer_class = RecordingSerializer
     http_method_names = ["get"]
     pagination_class = DjangoMusicBrainzConnectorPagination
+
+    def get_object(self):
+        """
+        Overriding the default `get_object` method of `ModelViewSet` to allow GET by any unique identifier for a Work,
+        either the MusicBrainz ID (an integer), or the GID (a UUID). Call with something like:
+
+            GET /api/recordings/1234/
+        """
+        pk = self.kwargs["pk"]
+        pk_type = _get_recording_identifier_type(pk)
+        params = {pk_type: pk}
+        try:
+            return self.serializer_class.Meta.model.objects.get(**params)
+        except self.serializer_class.Meta.model.DoesNotExist:
+            raise NotFound
--- django_musicbrainz_connector/tests/api/test_recording_api.py
+++ django_musicbrainz_connector/tests/api/test_recording_api.py
@@ -1,7 +1,20 @@
 import pytest
+from rest_framework.test import APIClient
 
 from django_musicbrainz_connector.api.recording import RecordingSerializer
 from django_musicbrainz_connector.models import Recording
+
+RECORDING_API_RESPONSE = {
+    "id": 13679939,
+    "gid": "fdb9d6f4-73f2-41f2-ba57-a8dbb65433a2",
+    "name": "Αχάριστη",
+    "length": 195000,
+    "comment": "",
+    "edits_pending": 0,
+    "last_updated": "2023-11-10T14:02:54.412000-06:00",
+    "video": False,
+    "artist_credit": 1002760,
+}
 
 
 @pytest.mark.django_db
@@ -10,3 +23,38 @@ def test_recording_serializer():
     serializer = RecordingSerializer(instance=instance)
 
     assert serializer.data["name"] == "Αχάριστη"
+
+
+@pytest.mark.django_db
+def test_recording_api_GET():
+    api_client = APIClient()
+    response = api_client.get("/api/recordings/")
+    assert response.status_code == 200
+    assert response.data["count"] == 1
+    assert response.data["next"] is None
+    assert response.data["previous"] is None
+    assert response.data["results"] == [RECORDING_API_RESPONSE]
+
+
+@pytest.mark.django_db
+def test_recording_api_GET_by_id():
+    api_client = APIClient()
+    response = api_client.get("/api/recordings/13679939/")
+    assert response.status_code == 200
+    assert response.data == RECORDING_API_RESPONSE
+
+
+@pytest.mark.django_db
+def test_recording_api_GET_by_gid():
+    api_client = APIClient()
+    response = api_client.get("/api/recordings/fdb9d6f4-73f2-41f2-ba57-a8dbb65433a2/")
+    assert response.status_code == 200
+    assert response.data == RECORDING_API_RESPONSE
+
+
+@pytest.mark.django_db
+def test_recording_api_GET_not_found():
+    api_client = APIClient()
+    response = api_client.get("/api/recordings/1/")
+    assert response.status_code == 404
+    assert response.data == {"detail": "Not found."}
