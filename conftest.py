f60fa4efe1a545b9d512b2fb1c6f8ecd511d45ee
Marios Zindilis <marios@zindilis.com>
2024-01-14 10:23:51 +0000

    Better loading for JSON fixtures

--- conftest.py
+++ conftest.py
@@ -19,21 +19,26 @@ def pytest_sessionstart():
 
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
+    """Load all JSON fixtures."""
+    fixtures = [
+        "link-type",
+        "link",
+        "work-type",
+        "work",
+        "artist-credit",
+        "recording",
+        "recording-work-link",
+        "script",
+        "language",
+        "release-packaging",
+        "release-status",
+        "release-group-primary-type",
+        "release-group",
+        "release",
+        "medium-format",
+        "medium",
+        "track",
+    ]
     with django_db_blocker.unblock():
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/link-type.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/link.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/work-type.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/work.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/artist-credit.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/recording.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/recording-work-link.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/script.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/language.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/release-packaging.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/release-status.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/release-group-primary-type.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/release-group.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/release.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/medium-format.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/medium.json")
-        call_command("loaddata", "django_musicbrainz_connector/tests/fixtures/track.json")
+        for fixture in fixtures:
+            call_command("loaddata", f"django_musicbrainz_connector/tests/fixtures/{fixture}.json")
