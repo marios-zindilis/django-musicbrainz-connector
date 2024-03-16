6f52f278a22ecf698f10e80043fa4d7278ce4faf
Marios Zindilis <marios@zindilis.com>
2024-03-16 10:11:01 +0000

    The ISO fields may be null on the Language model.

--- django_musicbrainz_connector/models/language.py
+++ django_musicbrainz_connector/models/language.py
@@ -15,12 +15,12 @@ from django.db import models
 
 class Language(models.Model):
     id = models.IntegerField("ID", primary_key=True, db_column="id")
-    iso_code_2t = models.CharField("ISO Code 2T", max_length=3, db_column="iso_code_2t")
-    iso_code_2b = models.CharField("ISO Code 2B", max_length=3, db_column="iso_code_2b")
-    iso_code_1 = models.CharField("ISO Code 1", max_length=2, db_column="iso_code_1")
+    iso_code_2t = models.CharField("ISO Code 2T", max_length=3, null=True, blank=True, db_column="iso_code_2t")
+    iso_code_2b = models.CharField("ISO Code 2B", max_length=3, null=True, blank=True, db_column="iso_code_2b")
+    iso_code_1 = models.CharField("ISO Code 1", max_length=2, null=True, blank=True, db_column="iso_code_1")
     name = models.CharField("Name", max_length=100, db_column="name")
     frequency = models.SmallIntegerField("Frequency", default=0, db_column="frequency")
-    iso_code_3 = models.CharField("ISO Code 3", max_length=3, db_column="iso_code_3")
+    iso_code_3 = models.CharField("ISO Code 3", max_length=3, null=True, blank=True, db_column="iso_code_3")
 
     def __str__(self):
         return self.name
